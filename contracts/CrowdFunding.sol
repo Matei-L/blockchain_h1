242d29bea67a60c95a0f78ceee8570b079ddcb8d
Cercel Irina <irinacercel@Irinas-MacBook-Pro.local>
2020-11-07 10:56:37 +0200

    created crowdFubding component

--- contracts/CrowdFunding.sol
+++ contracts/CrowdFunding.sol
@@ -1,2 +1,116 @@
 // "SPDX-License-Identifier: UNLICENSED"
 pragma solidity ^0.7.4;
+
+import "h1/Ownable.sol";
+
+contract CrowdFunding is Ownable {
+    
+    enum State { TARGET_NOT_REACHED, TARGET_REACHED, CLOSED }
+    State state;
+    
+    uint fundingGoal;
+    uint totalFundsReceived;
+    
+    constructor(uint _fundingGoal) {
+        
+        state = State.TARGET_NOT_REACHED;
+        fundingGoal = _fundingGoal;
+        totalFundsReceived = 0;
+    }
+    
+    struct Contributor {
+        string name;
+        uint age;
+        uint contribution;
+    }
+    mapping(address => Contributor) contributors;
+    
+    function distributeFunding() public onlyOwner {
+        
+        
+    }
+    
+    function contribute() public payable {
+        
+        if (state != State.TARGET_NOT_REACHED) {
+            revert("Target is already reached!");
+        }
+    
+        uint fundsToAdd;
+        if (totalFundsReceived + msg.value >= fundingGoal) {
+            
+            fundsToAdd = fundingGoal - totalFundsReceived;
+            uint rest = msg.value - fundsToAdd;
+            contributors[msg.sender].contribution = fundsToAdd;
+            
+            if (rest>0) {
+                msg.sender.transfer(rest);
+            }
+            
+            state = State.TARGET_REACHED;
+        } else {
+            
+            fundsToAdd = msg.value;
+            contributors[msg.sender].contribution = fundsToAdd;
+        }
+        
+        totalFundsReceived += fundsToAdd;
+    }
+    
+    function removeMoneyFromContribution(uint amount) public {
+        
+        if (state != State.TARGET_NOT_REACHED) {
+            revert("Target is already reached!");
+        }
+        
+        
+        if (contributors[msg.sender].contribution < amount) {
+            
+            revert("Retrieved money is bigger than contribution");
+        }
+        
+        contributors[msg.sender].contribution -= amount;
+        msg.sender.transfer(amount);
+        totalFundsReceived -= amount;
+    }
+    
+    function getState() public view returns (string memory) {
+        
+        if(state == State.TARGET_REACHED) {
+            
+            return "Target reached!";
+        } else if (state == CLOSED) {
+            
+            return "Target reached! Funding Closed!";
+        }
+        
+        return "Target not reached!";
+    }
+    
+    function getFundingGoal() public view returns (uint) {
+        
+        return fundingGoal;
+    }
+    
+    function getFundsReceived() public view returns (uint) {
+        
+        return totalFundsReceived;
+    }
+    
+    function getContributorForAddress(address contributorAddress) public view onlyOwner returns (string memory, uint, uint) {
+        
+        return (contributors[contributorAddress].name, contributors[contributorAddress].age, contributors[contributorAddress].contribution);
+    }
+    
+    function viewProfile() public view returns (string memory, uint, uint) {
+        
+        return (contributors[msg.sender].name, contributors[msg.sender].age, contributors[msg.sender].contribution);
+    }
+    
+    function editProfile(string memory name, uint age) public {
+        
+        contributors[msg.sender].name = name;
+        contributors[msg.sender].age = age;
+    }
+    
+}
